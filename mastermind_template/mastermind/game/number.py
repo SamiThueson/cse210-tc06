f42dfab1fca0cb8292300db73aa5e6a2cc73ec52
comfortazuonwu <commychi029@gmail.com>
2021-05-29 18:50:47 -0600

    hint and number class

---
+++ mastermind_template/mastermind/game/number.py
@@ -0,0 +1,15 @@
+import random
+class Number:
+    """This class gets the random number from a list and stores it in a variable
+    Attributes:
+    guess: The integer to be guesses
+    """
+    def __init__(self,guess):
+        self.guess = random.randint[1000,9999]
+
+
+    def set_number(self):
+        number = self.guess
+
+        return number
+    
