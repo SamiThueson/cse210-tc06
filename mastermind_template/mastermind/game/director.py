54feb99c92cd6c2e8869f477e0abddb314a33904
dappletonj <app19002@byui.edu>
2021-05-29 21:47:06 -0600

    director and guess class

---
+++ mastermind_template/mastermind/game/director.py
@@ -0,0 +1,58 @@
+from game.guess import Guess
+
+class Director:
+    """A code template for a person who directs the game. The responsibility of 
+    this class of objects is to keep track of the score and control the 
+    sequence of play.
+    
+    Attributes:
+        keep_playing (boolean): Whether or not the player wants to keep playing.
+        score (number): The total number of points earned.
+        thrower (Thrower): An instance of the class of objects known as Thrower.
+    """
+
+    def __init__(self):
+        """The class constructor.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        pass
+
+    def start_game(self):
+        """Starts the game loop to control the sequence of play.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        while self.keep_playing:
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
+
+    def get_inputs(self):
+        """Gets the inputs at the beginning of each round of play. In this case,
+        that means throwing the dice.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        pass
+        
+    def do_updates(self):
+        """Updates the important game information for each round of play. In 
+        this case, that means updating the score.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        pass
+        
+    def do_outputs(self):
+        """Outputs the important game information for each round of play. In 
+        this case, that means the dice that were rolled and the score.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        pass
